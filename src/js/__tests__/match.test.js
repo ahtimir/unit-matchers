5598bfe247373bf0393c7e06d70432221a13c1ea
ahtimir <ahtimir@gmail.com>
2020-08-20 22:31:29 +0300

    added tests

--- src/js/__tests__/match.test.js
+++ src/js/__tests__/match.test.js
@@ -17,7 +17,44 @@ test('match toEqual test (Deep Equality)', () => {
   expect(sorted).toEqual(squadSorted);
 });
 
-/*
+test('match  test (even health)', () => {
+  const squad = [
+    { name: 'мечник', health: 50 },
+    { name: 'маг', health: 50 },
+    { name: 'лучник', health: 50 },
+  ];
+  const squadSorted = [
+    { name: 'мечник', health: 50 },
+    { name: 'маг', health: 50 },
+    { name: 'лучник', health: 50 },
+  ];
+  const sorted = sortHeroes(squad);
+  expect(sorted).toEqual(squadSorted);
+});
+
+test('match toEqual test (7 person squad)', () => {
+  const squad = [
+    { name: 'лучник', health: 80 },
+    { name: 'маг', health: 10 },
+    { name: 'мечник', health: 20 },
+    { name: 'рыцарь', health: 60 },
+    { name: 'вор', health: 50 },
+    { name: 'паладин', health: 40 },
+    { name: 'нежить', health: 90 },
+  ];
+  const squadSorted = [
+    { name: 'нежить', health: 90 },
+    { name: 'лучник', health: 80 },
+    { name: 'рыцарь', health: 60 },
+    { name: 'вор', health: 50 },
+    { name: 'паладин', health: 40 },
+    { name: 'мечник', health: 20 },
+    { name: 'маг', health: 10 },
+  ];
+  const sorted = sortHeroes(squad);
+  expect(sorted).toEqual(squadSorted);
+});
+
 test('match toBe test', () => {
   const squad = [
     { name: 'мечник', health: 10 },
@@ -30,6 +67,5 @@ test('match toBe test', () => {
     { name: 'мечник', health: 10 },
   ];
   const sorted = sortHeroes(squad);
-  expect(sorted).toBe(squadSorted);
+  expect(sorted).not.toBe(squadSorted);
 });
-*/
